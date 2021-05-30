933f5dc5e5753b6de2321d216d82c17c1f98c491
Anthony <karam.antho@gmail.com>
2021-05-30 19:13:12 +0300

    step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,11 @@
+function multiply(valInput1, valInput2) {
+
+    var valInput1 = document.getElementById("first_number").value;
+    var valInput2 = document.getElementById("second_number").value;
+    var sum = valInput1 * valInput2;
+    document.getElementById("result").innerHTML = sum;
+}
+
+document.querySelector('#validate').addEventListener('click', function(event) {
+    event.preventDefault();
+});
