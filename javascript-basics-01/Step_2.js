17f433adfde450083d7170a562a51ef09c7ade3f
Anthony <karam.antho@gmail.com>
2021-05-30 17:27:47 +0300

    step_2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,4 @@
+// document.write("<p>Nom:John</p><p>Nom:John</p><p>Nom:John</p>")
+document.getElementById("list").innerHTML = "Nom : John<br>Prenom : Doe<br>Ville : New York";
+document.getElementById("list").style.marginLeft = "180px";
+document.getElementById("list").style.marginTop = "80px";
