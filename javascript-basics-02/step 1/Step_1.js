047ab58c9a10eb5a54569d7d8ed8e958fb6e471a
Anthony <karam.antho@gmail.com>
2021-05-30 22:10:26 +0300

    step_1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,6 @@
+// document.querySelector("image1").addEventListener("mousonver",function (){
+
+var firstTarget = document.querySelector("#image1");
+firstTarget.addEventListener("mouseover", function() {
+    this.src = "./images/image1_2.jpg"
+});
