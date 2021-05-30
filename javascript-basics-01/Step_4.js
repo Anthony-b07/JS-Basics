b9bc23ccdebda49cdce1ce9a05c58ed0f3c15bf6
Anthony <karam.antho@gmail.com>
2021-05-30 18:09:43 +0300

    step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,10 @@
+function getHacked() {
+    var inputVal1 = document.getElementById("name").value;
+    var inputVal2 = document.getElementById("surname").value;
+    var inputVal3 = document.getElementById("city").value;
+    alert(`
+    ${inputVal1} 
+    ${inputVal2}
+    ${inputVal3}
+    You've been HACKED`);
+}
