afc77f7c3009d0650574b7132a583437d7bd1d19
Anthony <karam.antho@gmail.com>
2021-05-31 00:01:17 +0300

    step_5

--- javascript-basics-02/step 5/step_5.js
+++ javascript-basics-02/step 5/step_5.js
@@ -18,23 +18,3 @@ var firstTarget = document.getElementById("image5");
 firstTarget.addEventListener("mouseover", function() {
     this.src = "./images/image4.jpg";
 });
-var firstTarget = document.getElementById("image1");
-firstTarget.addEventListener("mouseout", function() {
-    this.src = "./images/image1.jpg";
-});
-var firstTarget = document.getElementById("image2");
-firstTarget.addEventListener("mouseout", function() {
-    this.src = "./images/image2.jpg";
-});
-var firstTarget = document.getElementById("image3");
-firstTarget.addEventListener("mouseout", function() {
-    this.src = "./images/image3.jpg";
-});
-var firstTarget = document.getElementById("image4");
-firstTarget.addEventListener("mouseout", function() {
-    this.src = "./images/image4.jpg";
-});
-var firstTarget = document.getElementById("image5");
-firstTarget.addEventListener("mouseout", function() {
-    this.src = "./images/image5.jpg";
-});
